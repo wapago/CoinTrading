138e3323dc3d607e36c632a6c047b9ffa6cd9a80
Kim Chanho <beroso20th@gmail.com>
2025-10-29 19:44:21 +0900

    미들웨어추가, 일부코드 리팩토링

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import requests
+import secrets
+
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from uvicorn import run
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 
 from domain.v1 import binance, bitget, blockfin
-from domain.v1.blockfin.router import generate_signature, auth_headers
-from config import static_path, symbol_obj, BLOCKFIN_BASE_URL, SYMBOL_LIST
+from domain.v1.blockfin.router import generate_signature, set_auth_headers
+from config import symbol_obj, BLOCKFIN_BASE_URL, SYMBOL_LIST
+
+
+secret_key = secrets.token_hex(32)
+session_max_age = 60 * 60 * 24 * 90
 
 
 def set_symbols(symbol_list: list):
@@ -15,14 +22,14 @@ def set_symbols(symbol_list: list):
         query_params = {"instId": symbol}  # BTC-USDT
         request_path = '/api/v1/market/instruments'
         timestamp, nonce, query_string, signature = generate_signature(method='GET', request_path=request_path, query_params=query_params)
-        headers = auth_headers(signature, timestamp, nonce)
+        headers = set_auth_headers(signature, timestamp, nonce)
         response_json = requests.get(url=f"{BLOCKFIN_BASE_URL}{request_path}?{query_string}", headers=headers).json()
         return_obj[symbol] = response_json
     return return_obj
 
 
 @asynccontextmanager
-async def lifespan(_app: FastAPI):
+async def set_symbols(_app: FastAPI):
     symbol_obj.update(set_symbols(SYMBOL_LIST))
     yield
     print("=========== SERVER SHUTDOWN ===========")
@@ -31,10 +38,16 @@ async def lifespan(_app: FastAPI):
 app = FastAPI(
     title='CoinTrading_test',
     version='0.0.1',
-    lifespan=lifespan
+    lifespan=set_symbols
+)
+app.add_middleware(SessionMiddleware, secret_key=secret_key, max_age=session_max_age, same_site="strict")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 프론트엔드 도메인
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-app.mount('/static', StaticFiles(directory=static_path), name='static')
 app.include_router(binance.router)
 app.include_router(bitget.router)
 app.include_router(blockfin.router)
